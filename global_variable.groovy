6e031a6df570e8a88cbffb4c9dc31ccb9fe12ae5
Hien Luong Tran Hy <hyhien@gmail.com>
2020-08-09 21:35:32 +0700

    Create global_variable.groovy

---
+++ global_variable.groovy
@@ -0,0 +1,17 @@
+pipeline {
+  agent any
+  stages {
+    stage('Build') {
+      steps {
+        echo "We are in build ${currentBuild.number}"
+        echo "Our current result is ${currentBuild.currentResult}"
+      }
+    }
+    stage('BuildMore') {
+      steps {
+        echo "We are in build ${currentBuild.number}"
+        echo "Our current result is ${currentBuild.currentResult}"
+      }
+    }
+  }
+}
